d4e858afac145275d103331cc1d2c841797e6ecf
qualquercois1 <cassiodossantosrodrigues@gmail.com>
2025-07-15 23:55:54 -0300

    criacao do form

--- pages/gerador.py
+++ pages/gerador.py
@@ -8,20 +8,37 @@ st.set_page_config(
 
 st.title('Gerador')
 
-with st.form('Gerador'):
+with st.expander('Gerador', expanded=True):
     st.subheader('Formulario')
     st.info('Os campos são opcionais.')
 
     col1, col2 = st.columns(2)
     with col1:
-        classe = st.selectbox(
-            'Escolha uma classe:',
-            ['Opção 1', 'Opção 2', 'Opção 3']
-        )
-    with col2:
-        email = st.text_input('E-mail')
+        nome = st.text_input('Nome')
 
-    enviar = st.form_submit_button('Enviar')
+        classe = st.selectbox(
+            'Classe:',
+            ["", "Guerreiro(a)", "Mago(a)", "Ladrão(a)", "Clérigo(a)", "Bárbaro(a)", "Bardo", "Patrulheiro(a)", "Druida", "Outro..."]
+        )
+        if classe == 'Outro...':
+            classe = st.text_input('Digite a Classe desejada')
+
+        raca = st.selectbox(
+            'Raça',
+            ["", "Humano(a)", "Elfo(a)", "Anão(ã)", "Orc", "Halfling", "Meio-Elfo(a)", "Draconato(a)", "Outro..."]
+        )
+        if raca == 'Outro...':
+            raca = st.text_input('Digite a raça desejada')
+
+    with col2:
+        altura = st.number_input("Altura", placeholder="Ex: 1,80m", min_value=0.0)
+
+        fisico = st.selectbox(
+            "Fisico",
+            ["", "Magro(a)", "Atlético(a)", "Robusto(a)", "Normal"]
+        )
+
+    enviar = st.button('Enviar')
 
 if enviar:
     st.success(f"Obrigado! Sua mensagem foi enviada com sucesso.")
